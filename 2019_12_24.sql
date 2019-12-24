379882eca63eee9eddde388b0a73731acf5803c9
JOO <ahert3366@gmail.com>
2019-12-24 12:47:50 +0900

    PL/SQL
    "PL/SQL
     . 명시적 커서와 FOR LOOP
     . 인자가 있는 명시적 커서
     . FOR LOOP에서 커서 인라인 선언
    
    PL/SQL 실습
     . PRO_3
     . PRO_3 SQL로 작성하기 
        1. rownum
        2. 분석함수 
        3. 그룹함수
     . PRO_4
     , INSERT SELECT 절로 부하 줄이기"

--- 2019_12_24.sql
+++ 2019_12_24.sql
@@ -75,14 +75,36 @@ SELECT *
 FROM cycle;
 
 CREATE OR REPLACE PROCEDURE CREATE_DAILY_SALES
-(v_yyyymm IN VARCHAR2)IS
+(p_yyyymm IN VARCHAR2)IS
+    TYPE cal_row_type IS RECORD(
+    dt VARCHAR2(8),
+    day NUMBER);
+    
+    TYPE cal_tab IS TABLE OF cal_row_type INDEX BY BINARY_INTEGER;
+    p_cal_tab cal_tab;
 
 BEGIN
     --생성하기전에 해당년월에 해당하는 일시적 데이터를 삭제한다.
     DELETE daily
-    WHERE dt LIKE v_yyyymm || '%';
+    WHERE dt LIKE p_yyyymm || '%';
     
+    --달력정보를 table변수에 저장한다.
+    --반복적인 sql 실행을 방지하기 위해 한번만 실행해서 변수에 저장
+    SELECT  TO_CHAR(TO_DATE(p_yyyymm,'YYYYMM') + (LEVEL-1),'YYYYMMDD')dt,
+            TO_CHAR(TO_DATE(p_yyyymm,'YYYYMM') + (LEVEL-1),'D')day
+        BULK COLLECT INTO p_cal_tab
+    FROM dual
+    CONNECT BY LEVEL <= TO_CHAR(LAST_DAY(TO_DATE(p_yyyymm,'YYYYMM')),'DD');
+    
+    --애음주기 정보를 읽는다
     FOR daily IN (SELECT * FROM cycle) LOOP
+        --12월 일자달력 : cycle row 건수만큼 반복
+        FOR i IN 1..p_cal_tab.count LOOP
+            IF daily.day = p_cal_tab(i).day THEN
+            --cid,pid, 일자, 수량
+            INSERT INTO daily VALUES(daily.cid,daily.pid,p_cal_tab(i).dt,daily.cnt);
+            END IF;
+        END LOOP;
         DBMS_OUTPUT.PUT_LINE(daily.cid ||','||daily.day);
     END LOOP;
 END;
@@ -90,11 +112,18 @@ END;
 exec CREATE_DAILY_SALES('201912');
 
 --
-SELECT  TO_CHAR(TO_DATE('201912','YYYYMM') + (LEVEL-1),'YYYYMMDD')dt,
-        TO_CHAR(TO_DATE('201912','YYYYMM') + (LEVEL-1),'D')day
-FROM dual
-CONNECT BY LEVEL <=TO_CHAR(LAST_DAY(TO_DATE('201912','YYYYMM')),'DD');
+    SELECT cycle.cid, cycle.pid, cal.dt ,cycle.cnt
+    FROM cycle,
+    (SELECT  TO_CHAR(TO_DATE(:p_yyyymm,'YYYYMM') + (LEVEL-1),'YYYYMMDD')dt,
+            TO_CHAR(TO_DATE(:p_yyyymm,'YYYYMM') + (LEVEL-1),'D')day
+    FROM dual
+    CONNECT BY LEVEL <=TO_CHAR(LAST_DAY(TO_DATE(:p_yyyymm,'YYYYMM')),'DD'))cal
+    
+    WHERE cycle.day = cal.day;
 
 SELECT *
-FROM cycle;
+FROM daily;
+
+INSERT DAILY VALUES ( 1, 100, '20191201' ,5);
+
 
